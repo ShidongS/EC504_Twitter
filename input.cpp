abd525dcb9f9b125e80515f6e2c0124db6aca0ea
ShidongS <43020747+ShidongS@users.noreply.github.com>
2019-12-01 17:46:55 -0500

    new reading method

--- input.cpp
+++ input.cpp
@@ -11,20 +11,20 @@ void setUpdatabase(string word, int i){
 int main()
 {
 	vector<string> Twits;      
-	string line;
+	char line[300];
 	int i = 0;
-	ifstream myfile ("SampleInputForProject.txt");
+	ifstream myfile ("abcnews-date-text.csv");
 	if (myfile.is_open())
   	{
-    	while ( getline (myfile,line) )
+    	while ( myfile.getline(line, 300, '\n') )
     	{
       		//cout << line << '\n';
       		Twits.push_back(line);
-      		char * str = new char [line.length()+1];;
-      		strcpy (str, line.c_str());
-      		char * rest = str;
+      		//char * str = new char [line.length()+1];;
+      		//strcpy (str, line.c_str());
+      		char * rest = line;
       		char * p;
-      		while ((p = strtok_r(rest, " ,.?!:;", &rest)))
+      		while ((p = strtok_r(rest, " ,.?!:;-", &rest)))
       		{
       			setUpdatabase(p,i);
       			//cout<<p<<"	"<<i<<endl;
